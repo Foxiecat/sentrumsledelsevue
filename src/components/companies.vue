1e05c2e32903d236b937851d7d18292666ff63f7
Danica Ellefsen Kvilhaug <65165280+Gloxie@users.noreply.github.com>
2020-09-08 09:56:25 +0200

    Prototype in Vue.js

---
+++ src/components/companies.vue
@@ -0,0 +1,58 @@
+<template>
+<!--<div>
+    <table v-bind:key="company" v-for="company in companies">
+        <tr>
+            <td>{{ company.title }} <button on:click="companyAbout">O</button></td>
+            <td>{{ company.lastUpdated }}</td>
+            <td>{{ company.branch }}</td>
+        </tr>
+    </table>
+</div>-->
+
+<div style="width: 600px">
+    <vue-table-dynamic :params="params" ref="table"></vue-table-dynamic>
+</div>
+</template>
+
+<script>
+import VueTableDynamic from 'vue-table-dynamic'
+// import CompanyItem from "./CompanyItem"
+
+export default {
+    name: "Companies",
+    components: {
+        VueTableDynamic
+    },
+    props: ["companies", "company"],
+    data() {
+        return {
+            params: {
+                data: [
+                    ['Index', 'Navn', 'Sist Oppdatert', 'Bransje'],
+                    [1, `Komplett`, '4', 'Electro'],
+                    [2, 'Mat Butikk', '10', 'Grocery Store'],
+                    [3, '63788d', 'a8c325', 'aab418'],
+                    [4, 'hf7y8c', '4rghjk', 'cgnhik']
+                ],
+                header: 'row',
+                border: true,
+                stripe: true,
+                enableSearch: true
+            }
+        }
+    }
+}
+</script>
+
+<style scoped>
+table,
+th,
+td {
+    border: 1px solid black;
+    padding: 5px;
+}
+
+div {
+    display: inline-block;
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,48 @@
+<template>
+<div id="home">
+    <Header />
+    <router-view />
+    <Companies v-bind:companies="company" />
+
+</div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Header from "../components/layout/header"
+import Companies from "../components/companies"
+
+export default {
+    name: 'Home',
+    components: {
+        Header,
+        Companies
+    },
+    data() {
+        return {
+            company: [{
+                    id: 1,
+                    title: "Komplett",
+                    lastUpdated: 4,
+                    branch: "Electro"
+                },
+                {
+                    id: 2,
+                    title: "Mat Butikk",
+                    lastUpdated: 10,
+                    branch: "Mat"
+                }
+            ]
+        }
+    }
+}
+</script>
+
+<style scoped>
+table,
+th,
+td {
+    border: 1px solid black;
+    padding: 5px;
+}
+</style>
